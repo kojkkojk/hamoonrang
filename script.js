761f6f1c3c36055735b0f11c55ccadd142afb6cf
최정훈/Hamoonrang <hamoonrang@gmail.com>
2021-12-10 16:09:28 +0900

    추가

--- script.js
+++ script.js
@@ -13,8 +13,14 @@ function showing(arr) {
 };
 function acc() { list.innerHTML = showing(dcConsData) };
 var dcConsData = [
-   { name: "mollu.jpg", keywords: ["몰루"], tags: ["몰?루"] },
-   { name: "화랑몰루.jpg", keywords: ["화랑몰루"], tags: ["몰?루"] },
-   { name: "주톱호랑이2.gif", keywords: ["댄스호랑2"], tags: ["댄스"] },
+	{name:"화랑몰루.jpg",keywords:["화랑몰루"],tags:["몰?루"]},
+	{name:"주톱호랑이2.gif",keywords:["댄스호랑2"],tags:["댄스"]},
+	{name:"개구리갑분싸.gif",keywords:["페페실망"],tags:["페페"]},
+	{name:"개구리빵야1.png",keywords:["페페권총"],tags:["페페"]},
+	{name:"개구리빵야2.png",keywords:["페페권총2"],tags:["페페"]},
+	{name:"더블앵무.gif",keywords:["더블앵무"],tags:["앵무"]},
+	{name:"흠.png",keywords:["흠"],tags:["흠"]},
+	{name:"흠회전3.gif",keywords:["회전흠"],tags:["흠"]},
 ];
+
 acc();
