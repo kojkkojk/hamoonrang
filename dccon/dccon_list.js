7534d56911150c381972b3e9b6cbd15fcc1bc658
최정훈/Hamoonrang <hamoonrang@gmail.com>
2021-12-10 16:09:42 +0900

    추가

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -1,5 +1,4 @@
 ﻿dcConsData = [
-	{name:"mollu.jpg",keywords:["몰루"],tags:["몰?루"]},
 	{name:"화랑몰루.jpg",keywords:["화랑몰루"],tags:["몰?루"]},
 	{name:"주톱호랑이2.gif",keywords:["댄스호랑2"],tags:["댄스"]},
 	{name:"개구리갑분싸.gif",keywords:["페페실망"],tags:["페페"]},
