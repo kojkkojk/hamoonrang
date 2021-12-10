6fe4373d8c87317c95fce88f5093c81f190d5ee5
최정훈/Hamoonrang <hamoonrang@gmail.com>
2021-12-10 14:35:34 +0900

    수정

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -1,5 +1,5 @@
 ﻿dcConsData = [
 	{name:"mollu.jpg",keywords:["몰루"],tags:["몰?루"]},
-	{name:"화랑몰루.jpg",keywords:["화랑몰루"],tags:["소닉"]},
-	{name:"주톱호랑이2.gif",keywords:["댄스호랑2"],tags:["와우림"]},
+	{name:"화랑몰루.jpg",keywords:["화랑몰루"],tags:["몰?루"]},
+	{name:"주톱호랑이2.gif",keywords:["댄스호랑2"],tags:["댄스"]},
 ];
