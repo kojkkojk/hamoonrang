85955076555a827c9dfeb74bd2894d26fc3100af
최정훈/Hamoonrang <47581521+kojkkojk@users.noreply.github.com>
2021-12-11 09:29:47 +0900

    스크립트업데이트

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ function showing(arr) {
    return tag
 };
 function acc() { list.innerHTML = showing(dcConsData) };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var dcConsData = [
 	{name:"화랑몰루.jpg",keywords:["화랑몰루"],tags:["몰?루"]},
 	{name:"주톱호랑이2.gif",keywords:["댄스호랑2"],tags:["댄스"]},
@@ -21,6 +37,14 @@ var dcConsData = [
 	{name:"더블앵무.gif",keywords:["더블앵무"],tags:["앵무"]},
 	{name:"흠.png",keywords:["흠"],tags:["흠"]},
 	{name:"흠회전3.gif",keywords:["회전흠"],tags:["흠"]},
+	{name:"제리감사.gif",keywords:["제리감사"],tags:["제리"]},
+	{name:"1등급.jpg",keywords:["1등급"],tags:["아잇"]},
+	{name:"로치.jpg",keywords:["외않됨?"],tags:["유희왕"]},
+	{name:"무효아님.jpg",keywords:["싸이크론"],tags:["유희왕"]},
+	{name:"자일리통.jpg",keywords:["300원"],tags:["철권"]},
+	{name:"콩콩2.jpg",keywords:["콩콩이"],tags:["롤"]},
 ];
+
+
 
 acc();
